1af88f1d8daef9176489a7b6664fcba14aaab2b9
zainhaq34 <zainhaq@outlook.com>
2019-05-04 01:45:24 +0500

    Update HomeFragment.kt

--- LEDScreenCalculator/app/src/main/java/com/example/ledscreencalculator/Fragments/HomeFragment.kt
+++ LEDScreenCalculator/app/src/main/java/com/example/ledscreencalculator/Fragments/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.ledscreencalculator.R
-
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 /**
@@ -19,11 +19,12 @@ import com.example.ledscreencalculator.R
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
     }
 
 
